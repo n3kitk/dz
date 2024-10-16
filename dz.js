242d6d8570825c60dce7223a269c0e0d179f086c
Nikita2007uu <148580169+nkknickk@users.noreply.github.com>
2024-10-16 21:42:15 +0300

    main
    main

---
+++ dz.js
@@ -0,0 +1,41 @@
+const age = 25;
+console.log(age);
+
+const name = "Олександр";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Моя улюблена цитата.";
+console.log(myString);
+
+let myNumber = 42;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const nick = prompt ("Введіть ім'я");
+alert("Привіт" + name)
+console.log("Привіт" +name)
+
+const isConfirmed = confirm("Ви хочете підтвердити дію?");
+if (isConfirmed) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+
+alert("Увага! Це може бути небезпечно.");
+const confirmAction = confirm("Ви впевнені, що хочете продовжити?");
+if (confirmAction) {
+    alert("Дякую за підтвердження!");
+} else {
+    alert("Дію відмінено!");
+}
+
+
+
+
